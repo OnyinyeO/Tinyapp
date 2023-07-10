c7e57a825a7df76a8ac8a43bd18582ed2dd834e9
Onyinye Ochi <osemene.go@gmail.com>
2023-07-10 23:31:14 +0000

    created a new file

--- express_server.js
+++ express_server.js
@@ -24,3 +24,19 @@ app.get('/urls.json', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.get('/urls/:id', (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+
+  const templateVars = {
+    id,
+    longURL,
+  };
+  res.render('urls_show', templateVars);
+});
