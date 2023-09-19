0fb40307047c18635a19f1e0546dbae2b6ab28b4
Onyinye Ochi <Osemene.go@gmail.com>
2023-09-19 18:24:28 -0400

    set ejs as the view engine

--- express_server.js
+++ express_server.js
@@ -38,6 +38,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
@@ -46,6 +47,10 @@ const urlDatabase = {
 
 app.get('/', (req, res) => {
   res.send('Hello!');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.listen(PORT, () => {
